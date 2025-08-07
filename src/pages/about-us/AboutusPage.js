1ea8313552c754e4de1aa0ede7c434c91e87ef1a
rashedadwan99 <rashedadwan56789@gmail.com>
2025-08-08 01:01:46 +0300

    first commit

--- src/pages/about-us/AboutusPage.js
+++ src/pages/about-us/AboutusPage.js
@@ -4,16 +4,6 @@ import SectionLayout from "../../components/layout/section/SectionLayout";
 function AboutusPage() {
   return (
     <>
-      {/* <Writer sentence="about us" /> */}
-      <SectionLayout title="about atlas" dataaos="fade-up"></SectionLayout>
-      <SectionLayout title="categories" dataaos="fade-up"></SectionLayout>
-      <SectionLayout title="sponsors" dataaos="fade-up"></SectionLayout>
-      <SectionLayout title="about atlas" dataaos="fade-up"></SectionLayout>
-      <SectionLayout title="categories" dataaos="fade-up"></SectionLayout>
-      <SectionLayout title="sponsors" dataaos="fade-up"></SectionLayout>
-      <SectionLayout title="about atlas" dataaos="fade-up"></SectionLayout>
-      <SectionLayout title="categories" dataaos="fade-up"></SectionLayout>
-      <SectionLayout title="sponsors" dataaos="fade-up"></SectionLayout>
       <SectionLayout title="about atlas" dataaos="fade-up"></SectionLayout>
       <SectionLayout title="categories" dataaos="fade-up"></SectionLayout>
       <SectionLayout title="sponsors" dataaos="flip-up"></SectionLayout>
